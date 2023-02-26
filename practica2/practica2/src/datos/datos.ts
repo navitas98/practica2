ffe806c24118bbf81028812617f60bd19c12a506
jnavas98 <36570373+navitas98@users.noreply.github.com>
2023-02-26 01:18:57 +0100

    practica2 cambios hechos

---
+++ practica2/practica2/src/datos/datos.ts
@@ -0,0 +1,49 @@
+const destino = [
+    {
+        id: 1,
+        titulo:"Madrid",
+        descripcion:"Madrid es la capital de España, conocida por su rica historia, arte y gastronomía, así como por sus animados bares y tiendas.",
+        descripcionLarga:"Viaje a Madrid para conocer la ciudad",
+        imagen_corta: "https://images.unsplash.com/photo-1539037116277-4db20889f2d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+        imagen_larga: "https://images.pexels.com/photos/670261/pexels-photo-670261.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+       
+        
+    },
+    {
+        id: 2,
+        titulo:"Málaga",
+        descripcion:"Málaga es una ciudad en la costa mediterránea de España, conocida por su hermoso clima, playas, arquitectura histórica y la casa natal de Pablo Picasso.",
+        descripcionLarga:"Viaje a Málaga para conocer la ciudad",
+        imagen_corta:"https://images.pexels.com/photos/1884417/pexels-photo-1884417.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        imagen_larga:"https://www.pexels.com/es-es/foto/lote-de-dosel-de-playa-1884417/"
+       
+    },
+    {
+        id: 3,
+        titulo:"New York",
+        descripcion:"Nueva York es una ciudad vibrante en la costa este de los Estados Unidos, conocida por sus rascacielos icónicos, museos de fama mundial, la Estatua de la Libertad y el Central Park, así como por su energía y diversidad cultural.",
+        descripcionLarga:"Viaje a New York para conocer la ciudad",
+        imagen_corta: "https://images.pexels.com/photos/290386/pexels-photo-290386.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        imagen_larga: "https://www.pexels.com/es-es/foto/edificios-2224861/"
+    },
+    {
+        id: 4,
+        titulo:"Tokio",
+        descripcion:"Tokio es la capital de Japón, conocida por su impresionante arquitectura, tecnología avanzada, moda, cultura pop y comida deliciosa, así como por ser una de las ciudades más pobladas del mundo.",
+        descripcionLarga:"Viaje a Tokio para conocer la ciudad",
+        imagen_corta: "https://images.pexels.com/photos/2506923/pexels-photo-2506923.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        imagen_larga:"https://www.pexels.com/es-es/foto/gente-caminando-por-la-calle-2506923/",
+       
+    },
+    {
+        id: 5,
+        titulo:"Paris",
+        descripcion:"París es la capital de Francia, conocida como la Ciudad del Amor y la Luz, por su elegante arquitectura, monumentos famosos como la Torre Eiffel y el Museo del Louvre, la moda y la cocina francesa refinada.",
+        descripcionLarga:"Lzzz",
+        imagen_corta: "https://images.unsplash.com/photo-1499856871958-5b9627545d1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+        imagen_larga: "https://www.pexels.com/es-es/foto/foto-de-angulo-bajo-de-la-torre-eiffel-699466/"
+     
+    }
+]
+
+export default destino;
